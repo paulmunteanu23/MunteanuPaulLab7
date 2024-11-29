2de56a67bccd670cd16add9a3cd9ae38200521cd
paulmunteanu23 <paulmunteanu2323@gmail.com>
2024-11-29 17:24:26 +0200

    .

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,28 @@
-﻿namespace MunteanuPaulLab7
+﻿using System;
+using MunteanuPaulLab7.Data;
+using System.IO;
+
+namespace MunteanuPaulLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
